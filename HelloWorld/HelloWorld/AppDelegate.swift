963ef0bd88d32a2cde5ee543d6baee0def69c896
Michael Remediakis <m_remedi@berkeley.edu>
2017-12-08 10:46:58 -0800

    Started creating hello world app

---
+++ HelloWorld/HelloWorld/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HelloWorld
+//
+//  Created by Michael Remediakis on 12/8/17.
+//  Copyright © 2017 Michael Remediakis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
