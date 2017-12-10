3ad21cbb177a732ba4ddbfae47e28a0d21d00984
Michael Remediakis <m_remedi@berkeley.edu>
2017-12-09 18:40:33 -0800

    Finished making the hello world app, about to start making a timer app

---
+++ EggTimer/EggTimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Michael Remediakis on 12/9/17.
+//  Copyright © 2017 Michael Remediakis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
